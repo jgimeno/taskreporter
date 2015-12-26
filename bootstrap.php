b2c99981cb5b0a9ea9237d0eb69d1509bed4ac53
Daniel Esteban <superporterilloman@gmail.com>
2015-12-26 13:40:10 +0100

    Add dependency injection

--- bootstrap.php
+++ bootstrap.php
@@ -2,17 +2,43 @@
 
 use Doctrine\ORM\EntityManager;
 use Doctrine\ORM\Tools\Setup;
+use League\Container\Container;
 
 require_once 'vendor/autoload.php';
 
+$container = new Container();
+
 $isDevMode = true;
 
-$config = Setup::createYAMLMetadataConfiguration(array(__DIR__."/config/yml/"), $isDevMode);
+$config = Setup::createYAMLMetadataConfiguration(array(__DIR__ . "/config/yml/"), $isDevMode);
 
 $conn = array(
     'driver' => 'pdo_sqlite',
     'path' => __DIR__ . '/db.sqlite',
 );
 
-$em = EntityManager::create($conn, $config);
+$container->share('entityManager', function () use ($conn, $config) {
+    return EntityManager::create($conn, $config);
+});
 
+$container
+    ->add('JGimeno\TaskReporter\Entity\WorkingDayRepositoryInterface', 'JGimeno\TaskReporter\Infrastructure\DoctrineWorkingDayRepository')
+    ->withArgument($container->get('entityManager'));
+
+$container
+    ->add('JGimeno\TaskReporter\App\Command\AddTaskHandler', function () use ($container) {
+        return new \JGimeno\TaskReporter\App\Command\AddTaskHandler($container->get('JGimeno\TaskReporter\Entity\WorkingDayRepositoryInterface'));
+    });
+
+$container->add('JGimeno\TaskReporter\App\Console\CreateTask', function () use ($container) {
+    return new \JGimeno\TaskReporter\App\Console\CreateTask('taskReporter:add', $container->get('JGimeno\TaskReporter\App\Command\AddTaskHandler'));
+});
+
+$container
+    ->add('JGimeno\TaskReporter\App\Command\ListTasksHandler', function () use ($container) {
+        return new \JGimeno\TaskReporter\App\Command\ListTasksHandler($container->get('JGimeno\TaskReporter\Entity\WorkingDayRepositoryInterface'));
+    });
+
+$container->add('JGimeno\TaskReporter\App\Console\ListTasks', function () use ($container) {
+    return new \JGimeno\TaskReporter\App\Console\ListTasks('taskReporter:list', $container->get('JGimeno\TaskReporter\App\Command\ListTasksHandler'));
+});
--- console.php
+++ console.php
@@ -4,11 +4,12 @@
 require __DIR__.'/vendor/autoload.php';
 require __DIR__.'/bootstrap.php';
 
-use JGimeno\TaskReporter\App\Console\CreateTask;
-use JGimeno\TaskReporter\App\Console\ListTasks;
 use Symfony\Component\Console\Application;
 
 $application = new Application();
-$application->add(new CreateTask());
-$application->add(new ListTasks());
+
+$application->add($container->get('JGimeno\TaskReporter\App\Console\CreateTask'));
+
+$application->add($container->get('JGimeno\TaskReporter\App\Console\ListTasks'));
+
 $application->run();
